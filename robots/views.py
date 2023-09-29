3bf84dfad23fbcbd8835a8df720b7685b398c4a4
Alexander Bugaev <iustusvir@gmail.com>
2023-09-29 23:44:35 +0300

    data of the new robot to db

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
+import re
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .validators import *
+from .models import Robot
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class RobotView(View):
+
+    def post(self, request):
+        request_body = json.loads(request.body)
+        validated_robot_data = {
+            'model': validate_robot_version_model(request_body.get('model')),
+            'version': validate_robot_version_model(request_body.get('version')),
+            'serial': request_body.get('model') + "-" + request_body.get('version'),
+            'created': request_body.get('created')
+        }
+        robot_obj = Robot.objects.create(**validated_robot_data)      
+        return JsonResponse({'Cоздан робот': {"model":robot_obj.model,
+                                              "version":robot_obj.version,
+                                              "created":robot_obj.created}},
+                                               json_dumps_params={'ensure_ascii': False},
+                                               content_type='application/json; charset=utf-8')
+        
