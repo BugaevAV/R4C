87cd2f40f95fa22c41836a5caeb246aec318f37d
Alexander Bugaev <iustusvir@gmail.com>
2023-09-29 23:47:19 +0300

    dislpay data about robots

--- robots/admin.py
+++ robots/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Robot
 
-# Register your models here.
+
+
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ('serial', 'model', 'version', 'created', )
+
+
+admin.site.register(Robot, RobotAdmin)
