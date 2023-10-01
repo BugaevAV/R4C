c0349ee6577d02a555a8b16ddc13f1e932130ae1
Alexander Bugaev <iustusvir@gmail.com>
2023-10-01 22:25:14 +0300

    remove validators.py

--- robots/validators.py
+++
@@ -1,8 +0,0 @@
-import re
-from django.core.exceptions import ValidationError
-
-
-def validate_robot_version_model(value):
-    if not re.match("([A-Z0-9]{2})", value):
-            raise ValidationError('Параметр должен содержать комбинацию двух букв и(или) цифр')
-    return value
